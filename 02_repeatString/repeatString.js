f943192017ebb905f49d1a166db63a7983dc893c
Gio325 <175448374+Gio325@users.noreply.github.com>
2025-03-22 17:31:34 +0100

    finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+let result = '';
+const repeatString = function(wordToRepeat, numOfTimes) {
+    if(numOfTimes < 0){
+        return "ERROR"
+    }
+    for(let i = 0; i < numOfTimes; i++){
+        result += wordToRepeat
+    }
+    return result
 };
 
 // Do not edit below this line
