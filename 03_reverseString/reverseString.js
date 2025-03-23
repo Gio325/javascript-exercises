1380dce309c1c91a69175b037b02edec501d13dc
Gio325 <175448374+Gio325@users.noreply.github.com>
2025-03-23 22:40:16 +0100

    finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+let splitstr = str.split('')
+let revstr = splitstr.reverse();
+let joinstr = revstr.join('');
+return joinstr
 
-};
+}
 
 // Do not edit below this line
 module.exports = reverseString;
