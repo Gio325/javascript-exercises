6f462e249969e43071eacc93f2a2614953620de4
Gio325 <175448374+Gio325@users.noreply.github.com>
2025-03-25 15:18:01 +0100

    finihed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,19 @@
-const removeFromArray = function(array, ...nums) {
-//prima creo un array che conterra' i numeri richiesti
-const newArray = [];
-//creo un ciclo forEach() che verifica se gli items contetuti in nums
-//compaiono anche in array
-array.forEach(item => {
-    if(!nums.includes(item)){
-        newArray.push(item)
-    }
-});
-return newArray
+const removeFromArray = function (array, ...nums) {
+    //prima creo un array che conterra' i numeri richiesti
+    const newArray = [];
+    //creo un ciclo forEach() che verifica gli items di array...
+    array.forEach(item => {
+        //se gli items di nums NON (!) includono gli items di array...
+        if (!nums.includes(item)) {
+            //li aggiungo a newArray
+            newArray.push(item)
+        }
+    });
+    //restituisco il valore di newArray
+    return newArray
 }
+
+
 // Do not edit below this line
 
 module.exports = removeFromArray;
