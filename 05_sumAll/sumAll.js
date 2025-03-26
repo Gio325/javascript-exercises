e235630adee58bad0d1588cc2c3641600bd5b9b7
Gio325 <175448374+Gio325@users.noreply.github.com>
2025-03-26 15:57:41 +0100

    added comments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,16 @@
 const sumAll = function(num1, num2) {
+    //creazione variabile che conterra' il risultato della somma
 let sum = 0;
+//controlla che l'input inserito sia accettabile, senno' restituisce ERROR
 if(num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2) ){
     return "ERROR"
+//controlla che il primo numero sia maggiore del secondo...
 }else if(num1 < num2){
 for(let i = num1; i < num2; i++){
     sum += i;
 }
 sum += num2;
+//se invece il secondo e' maggiore del primo...
 }else{
     for(let i = num2; i < num1; i++){
         sum += i;
