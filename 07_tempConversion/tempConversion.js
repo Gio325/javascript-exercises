72a77818b6c4d6fe1e3b8a8777658b757f9a3a9a
Gio325 <175448374+Gio325@users.noreply.github.com>
2025-03-26 16:49:19 +0100

    finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+temp = (temp - 32) / 1.8;
+temp = temp.toFixed(1);
+return Number(temp);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+temp = (temp * 1.8) + 32;
+temp = temp.toFixed(1);
+return Number(temp);
 };
 
 // Do not edit below this line
